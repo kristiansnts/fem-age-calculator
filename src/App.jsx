0d7ce44b79bf137c8914223d0716561e943210aa
kristiansnts <57317793+kristiansnts@users.noreply.github.com>
2024-01-06 17:09:41 +0700

    set up style guide

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <h1 className='text-3xl underline font-bold'>Vite + React</h1>
-      <div className="card">
+      <div className="card bg-smokey-grey">
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary-purple': 'hsl(259, 100%, 65%)',
+        'light-red': 'hsl(0, 100%, 67%)',
+        'off-white': 'hsl(0, 0%, 94%)',
+        'light-grey': 'hsl(0, 0%, 86%)',
+        'smokey-grey': 'hsl(0, 1%, 44%)'
+      }
+    },
   },
   plugins: [],
 }
