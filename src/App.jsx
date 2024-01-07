15534c53e5fd0f735082a0aa0aa8079d010bd997
kristiansnts <57317793+kristiansnts@users.noreply.github.com>
2024-01-07 11:02:51 +0700

    mobile style done

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,30 @@ import ArrowIcon from "images/icon-arrow.svg"
 function App() {
 
   return (
-    <main id="app">
-      <form id="inputs">
-        <div className="input__group">
-          <span>Day</span>
-          <input type="number" name="day" placeholder="DD"/>
+    <main id="app" className="h-screen flex items-center justify-center">
+      <div className="bg-white p-5 w-11/12 max-w-3xl rounded-t-xl rounded-bl-xl rounded-br-[80px]">
+      <form id="inputs" className="flex justify-between ">
+        <div className="input__group mr-3 w-25">
+          <span className="block mb-1 text-slate-500 text-xs tracking-widest uppercase">Day</span>
+          <input className="w-full border p-3 rounded-lg" type="number" name="day" placeholder="DD" />
         </div>
-        <div className="input__group">
-          <span>Month</span>
-          <input type="number" name="month" placeholder="MM"/>
+        <div className="input__group mr-3 w-25">
+          <span className="block mb-1 text-slate-500 text-xs tracking-widest uppercase">Month</span>
+          <input className="w-full border p-3 rounded-lg" type="number" name="month" placeholder="MM"/>
         </div>
-        <div className="input__group">
-          <span>Year</span>
-          <input type="number" name="year" placeholder="YYYY"/>
+        <div className="input__group mr-3 w-25">
+          <span className="block mb-1 text-slate-500 text-xs tracking-widest uppercase">Year</span>
+          <input className="w-full border p-3 rounded-lg" type="number" name="year" placeholder="YYYY"/>
         </div>
       </form>
-      <div className="separator">
-        <img src={ArrowIcon} alt="" className="bg-primary-purple" />
+      <div className="separator my-12 border-b relative cursor-pointer">
+        <img src={ArrowIcon} alt="" className="bg-primary-purple rounded-full w-12 p-3 absolute -top-5 right-1/2" />
       </div>
-      <div id="result">
-          <div className="year__result"><span>--</span> years</div>
-          <div className="month__result"><span>--</span> months</div>
-          <div className="year__result"><span>--</span> days</div>
+      <div id="result" className="p-3">
+          <div className="year__result text-5xl flex items-start italic font-bold"><span className="text-6xl text-primary-purple mr-3">--</span> years</div>
+          <div className="month__result text-5xl flex items-start italic font-bold"><span className="text-6xl text-primary-purple mr-3">--</span> months</div>
+          <div className="days__result text-5xl flex items-start italic font-bold"><span className="text-6xl text-primary-purple mr-3">--</span> days</div>
+      </div>
       </div>
     </main>
   )
